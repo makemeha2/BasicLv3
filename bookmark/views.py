0e7c3a958d01b9b84ddcc408998da92d2a3011c9
makemeha2 <makemeha2@gmail.com>
2021-04-06 23:45:23 +0900

    UI 수정 완료

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.urls import path
 from bookmark.models import Bookmark
 
 class BookmarkLV(ListView) : 
