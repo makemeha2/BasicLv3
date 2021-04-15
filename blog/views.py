0e28d79b20f98888bfaeb61cc782feea0b89e396
makemeha2 <makemeha2@gmail.com>
2021-04-16 00:29:44 +0900

    ??

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, TodayArchiveView
 from django.views.generic.detail import DetailView
 
+from django.conf import settings
+
 
 from blog.models import Post
 
@@ -14,6 +16,15 @@ class PostLV(ListView) :
 
 class PostDV(DetailView) :
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['disqus_short'] = f"{settings.DISQUS_SHORTNAME}"
+        context['disqus_id'] = f"post-{self.object.id}-{self.object.slug}"
+        context['disqus_url'] = f"{settings.DISQUS_MY_DOMAIN}{self.object.get_absolute_url()}"
+        context['disqus_title'] = f"{self.object.slug}"
+
+        return context
 
 class PostAV(ArchiveIndexView) :
     model = Post
