91dd82ea06f1ec71e0512ae3ee045e1c790d0315
makemeha2 <makemeha2@gmail.com>
2021-04-22 18:31:04 +0900

    회원가입

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,14 +21,15 @@ from django.conf import settings
 
 from bookmark.views import BookmarkLV, BookmarkDV
 from mysite.views import HomeView
+from mysite.views import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/', UserCreateDoneTV.as_View(), name='register_done'),
     path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
     path('photo/', include('photo.urls')),
-
-    # path('bookmark/', BookmarkLV.as_view(), name='index'),
-    # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
